57096e6c6ce8ddb4b791d95ad2ea4dc10cb21ece
j0verton <overtonje@gmail.com>
2021-02-19 15:56:20 -0600

    getting db connection working, added get all animals

--- animals/request.py
+++ animals/request.py
@@ -1,3 +1,8 @@
+import json
+import sqlite3
+from models import Animal
+
+
 ANIMALS = [
     {
         "id": 1,
@@ -27,8 +32,46 @@ ANIMALS = [
 
 
 def get_all_animals():
-    return ANIMALS
+   # Open a connection to the database
+    with sqlite3.connect("./kennel.db") as conn:
 
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.breed,
+            a.status,
+            a.location_id,
+            a.customer_id
+        FROM animal a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            animal = Animal(row['id'], row['name'], row['breed'],
+                            row['status'], row['location_id'],
+                            row['customer_id'])
+
+            animals.append(animal.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(animals)
     # Function with a single parameter
 
 
@@ -63,6 +106,7 @@ def create_animal(animal):
     # Return the dictionary with `id` property added
     return animal
 
+
 def delete_animal(id):
     # Initial -1 value for animal index, in case one isn't found
     animal_index = -1
@@ -77,6 +121,7 @@ def delete_animal(id):
     # If the animal was found, use pop(int) to remove it from list
     if animal_index >= 0:
         ANIMALS.pop(animal_index)
+
 
 def update_animal(id, new_animal):
     # Iterate the ANIMALS list, but use enumerate() so that
