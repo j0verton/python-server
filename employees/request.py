f65064ac8214fa76b5b934cad651f3a0a05b48fa
j0verton <overtonje@gmail.com>
2021-02-23 19:05:34 -0600

    added new sql db access functions for employees and locations get all and single

--- employees/request.py
+++ employees/request.py
@@ -1,3 +1,7 @@
+import json
+import sqlite3
+from models import Employee, location
+
 EMPLOYEES = [
     {
         "id": 1,
@@ -28,34 +32,60 @@ EMPLOYEES = [
 
 
 def get_all_employees():
-    return EMPLOYEES
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.location_Id
+        FROM employees e
+        """)
+        employees = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['location_id'])
+            employees.append(animal.__dict__)
+    return json.dumps(employees)
 
 
 def get_single_employee(id):
-    requested_employee = None
+        with sqlite3.connect("./kennel.db") as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+            db_cursor.execute("""
+            SELECT
+                e.id,
+                e.name,
+                e.location_Id
+            FROM employees e
+            WHERE e.id = ?
+            """, (id, ))
+            data = db_cursor.fetchall()
+            employee = Employee(data['id'], data['name'], data['location_id'])
+        return json.dumps(employee.__dict__)
 
-    for employee in EMPLOYEES:
-        if employee["id"] == id:
-            requested_employee = employee
-    return requested_employee
 
 def create_employee(employee)
-    max_id = EMPLOYEES[-1]["id"]
-    new_id = max_id +1
+  max_id = EMPLOYEES[-1]["id"]
+   new_id = max_id + 1
     employee["id"] = new_id
     EMPLOYEES.append(employee)
     return employee
 
+
 def delete_employee(id)
-    employee_index = -1
-    for index, employee in enumerate(EMPLOYEES):
+  employee_index = -1
+   for index, employee in enumerate(EMPLOYEES):
         if employee["id"] == id:
             employee = index
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
 
+
 def update_employees(id, new_employee)
-    for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
+  for index, employee in enumerate(EMPLOYEES):
+       if employee["id"] == id:
         EMPLOYEES[index] = new_employee
         break
--- locations/request.py
+++ locations/request.py
@@ -1,49 +1,62 @@
-LOCATIONS = [
-    {
-        "id": 1,
-        "name": "Nashville North",
-        "address": "8422 Johnson Pike"
-    },
-    {
-        "id": 2,
-        "name": "Nashville South",
-        "address": "209 Emory Drive"
-    },
-    {
-        "id": 3,
-        "name": "Uptown",
-        "address": "13049 Big St."
-    }
-]
+import json
+import sqlite3
+from models import Location
 
 
 def get_all_locations():
-    return LOCATIONS
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            l.id,
+            l.name,
+            l.address
+            FROM location l
+        """)
+
+        locations = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            location = Location(row['id'], row['name'], row['address'])
+            locations.append(location.__dict__)
+    return json.dumps(locations)
 
 
 def get_single_location(id):
-    requested_location = None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            l.id,
+            l.name,
+            l.address
+        FROM location l
+        WHERE l.id = ?
+        """, (id, ))
 
-    for location in LOCATIONS:
-        if location["id"] == location:
-            requested_location = location
+        data = db_cursor.fetchone()
+        location = Location(row['id'], row['name'], row['address'])
+        return json.dumps(location.__dict__)
 
-    return requested_location
 
 def create_location(location):
     max_id = LOCATIONS[-1]["id"]
-    new_id = max_id +1
+    new_id = max_id + 1
     location["id"] = new_id
     LOCATIONS.append(location)
     return location
 
+
 def delete_location(location):
-    location_index = -1 
-    for index, location in enumerate(LOCATIONS)
+    location_index = -1
+    for index, location in enumerate(LOCATIONS):
         if location["id"] == id:
             location_index = index
         if location_index >= 0:
             LOCATIONS.pop(location_index)
+
 
 def update_location(id, new_location):
     for index, location in enumerate(LOCATIONS):
