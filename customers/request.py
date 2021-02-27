d38b1f34ac23bdca64d6952233896c57fe2dd14b
j0verton <overtonje@gmail.com>
2021-02-27 14:45:37 -0600

    updated customers request with new methods and by email

--- customers/request.py
+++ customers/request.py
@@ -1,5 +1,6 @@
-from animals.request import ANIMALS
-
+import json
+import sqlite3
+from models import Customer
 
 CUSTOMERS = [
     {
@@ -29,17 +30,71 @@ CUSTOMERS = [
 
 
 def get_all_customers():
-    return CUSTOMERS
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.roe_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id
+            c.name,
+            c.email
+        FROM customer c
+        """)
+        customers = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            customer = Customer(row['id'], row['name'], row['email'])
+            customers.append(customer.__dict__)
+    return json.dumps(customers)
 
 
 def get_single_customer(id):
-    requested_customer = None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.roe_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    for customer in CUSTOMERS:
-        if customer["id"] == id:
-            requested_animal = ANIMALS
+        db_cursor.execute("""
+        SELECT
+            c.id
+            c.name,
+            c.email
+        FROM customer c
+        WHERE c.ie = ?
+        """)
+        data = db_cursor.fetchone()
+        customer = Customer(data['id'], data['name'], data['email'])
+        return json.dumps(customer.__dict__)
 
-    return requested_customer
+
+def get_customers_by_email(email):
+
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        select
+            c.id,
+            c.name,
+            c.address,
+            c.email,
+            c.password
+        from Customer c
+        WHERE c.email = ?
+        """, (email, ))
+
+        customers = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            customer = Customer(
+                row['id'], row['name'], row['address'], row['email'], row['password'])
+            customers.append(customer.__dict__)
+
+    return json.dumps(customers)
+
 
 def create_customer(customer):
     max_id = CUSTOMERS[-1]["id"]
@@ -47,6 +102,7 @@ def create_customer(customer):
     customer["id"] = new_id
     CUSTOMERS.append(customer)
     return customer
+
 
 def delete_customer(id):
     customer_index = -1
@@ -56,8 +112,9 @@ def delete_customer(id):
     if customer_index >= 0:
         CUSTOMERS.pop(customer-INDEX)
 
+
 def update_customers(id, new_customer):
     for index, customer in enumerate(CUSTOMERS)
-        if customer["id"] == id:
-            CUSTOMERS[index] = new_customer
-            break
+    if customer["id"] == id:
+        CUSTOMERS[index] = new_customer
+        break
