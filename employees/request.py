e14e0d4a1e7f0989dd3a9d3513e88c59a8493aec
j0verton <overtonje@gmail.com>
2021-02-28 13:37:33 -0600

    added employees by location

--- employees/request.py
+++ employees/request.py
@@ -46,46 +46,67 @@ def get_all_employees():
         dataset = db_cursor.fetchall()
         for row in dataset:
             employee = Employee(row['id'], row['name'], row['location_id'])
-            employees.append(animal.__dict__)
+            employees.append(employee.__dict__)
     return json.dumps(employees)
 
 
 def get_single_employee(id):
-        with sqlite3.connect("./kennel.db") as conn:
-            conn.row_factory = sqlite3.Row
-            db_cursor = conn.cursor()
-            db_cursor.execute("""
-            SELECT
-                e.id,
-                e.name,
-                e.location_Id
-            FROM employees e
-            WHERE e.id = ?
-            """, (id, ))
-            data = db_cursor.fetchall()
-            employee = Employee(data['id'], data['name'], data['location_id'])
-        return json.dumps(employee.__dict__)
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.location_Id
+        FROM employees e
+        WHERE e.id = ?
+        """, (id, ))
+        data = db_cursor.fetchall()
+        employee = Employee(data['id'], data['name'], data['location_id'])
+    return json.dumps(employee.__dict__)
 
 
-def create_employee(employee)
-  max_id = EMPLOYEES[-1]["id"]
-   new_id = max_id + 1
+def get_employees_by_location(location):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.location_Id
+        FROM employees e
+        WHERE e.location_id = ?
+        """, (location, ))
+        employees = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            employee = Employee(row['id'], row['name'], row['location_id'])
+            employees.append(employee.__dict__)
+    return json.dumps(employees)
+
+
+def create_employee(employee):
+    max_id = EMPLOYEES[-1]["id"]
+    new_id = max_id + 1
     employee["id"] = new_id
     EMPLOYEES.append(employee)
     return employee
 
 
-def delete_employee(id)
-  employee_index = -1
-   for index, employee in enumerate(EMPLOYEES):
+def delete_employee(id):
+    employee_index = -1
+    for index, employee in enumerate(EMPLOYEES):
         if employee["id"] == id:
             employee = index
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
 
 
-def update_employees(id, new_employee)
-  for index, employee in enumerate(EMPLOYEES):
-       if employee["id"] == id:
-        EMPLOYEES[index] = new_employee
-        break
+def update_employees(id, new_employee):
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            EMPLOYEES[index] = new_employee
+            break
